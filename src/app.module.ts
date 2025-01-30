d62c2b3ec62edb233d796289b31fccb86b0e941b
Btercero <byron.tercero@contollo.net>
2025-01-29 20:44:02 -0600

    add Joi validation schema for environment variables and update default values in configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,13 @@ import { SeedModule } from './seed/seed.module';
 import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
